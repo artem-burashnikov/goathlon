821d7c97a7df8010ffd3a3b19e38152023bce6f6
artem-burashnikov <artem.burashnikov@gmail.com>
2025-05-04 05:29:46 +0300

    feat: update config parser

--- config.go
+++ config.go
@@ -4,34 +4,69 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
-func Must[T any](obj T, err error) T {
-	if err != nil {
-		panic(err)
-	}
-	return obj
+type Config struct {
+	Laps        int      `json:"laps"`
+	LapLen      int      `json:"lapLen"`
+	PenaltyLen  int      `json:"penaltyLen"`
+	FiringLines int      `json:"firingLines"`
+	Start       Time     `json:"start"`
+	StartDelta  Duration `json:"startDelta"`
 }
 
-type Config struct {
-	Laps        int    `json:"laps"`
-	LapLen      int    `json:"lapLen"`
-	PenaltyLen  int    `json:"penaltyLen"`
-	FiringLines int    `json:"firingLines"`
-	Start       string `json:"start"`
-	StartDelta  string `json:"startDelta"`
+type Time struct {
+	time.Time
+}
+
+func (t *Time) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), `"`)
+	layouts := []string{
+		"15:04:05.000",
+		"15:04:05",
+	}
+	var err error
+	for _, layout := range layouts {
+		var parsed time.Time
+		parsed, err = time.Parse(layout, s)
+		if err == nil {
+			t.Time = parsed
+			return nil
+		}
+	}
+	return fmt.Errorf("invalid 'start' format: %s", s)
+}
+
+type Duration struct {
+	time.Duration
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), `"`)
+	t, err := time.Parse("15:04:05", s)
+	if err == nil {
+		d.Duration = time.Duration(t.Hour())*time.Hour +
+			time.Duration(t.Minute())*time.Minute +
+			time.Duration(t.Second())*time.Second
+		return nil
+	}
+	return fmt.Errorf("invalid 'startDelta' format: %s", s)
 }
 
 func loadConfig(path string) (Config, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return Config{}, fmt.Errorf("opening config file: %w", err)
+		return Config{}, err
 	}
 	defer file.Close()
 
 	var cfg Config
-	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
-		return Config{}, fmt.Errorf("decoding config JSON: %w", err)
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&cfg); err != nil {
+		return Config{}, fmt.Errorf("parsing config: %w", err)
 	}
+
 	return cfg, nil
 }
