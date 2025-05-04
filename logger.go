66e9cb7fe44bfeeeb0958926913d8921eeb611d9
artem-burashnikov <artem.burashnikov@gmail.com>
2025-05-04 22:49:07 +0300

    chore: remove redundant comments in logger.go

--- logger.go
+++ logger.go
@@ -5,12 +5,10 @@ import (
 	"io"
 )
 
-// logEvent writes a formatted event log message to the provided writer.
 func logEvent(w io.Writer, e Event) {
-	fmt.Fprintln(w, formatEventLog(e))
+	fmt.Fprintln(w, e)
 }
 
-// logError writes an error message to the provided writer.
 func logError(w io.Writer, kind string, err error) {
 	fmt.Fprintln(w, "[ERROR]", kind, "error has occured but was ignored:", err)
 }
