eef4d2bb5d8c1b814ee1771511cf4f1d8ad0bf72
artem-burashnikov <artem.burashnikov@gmail.com>
2025-05-04 05:30:24 +0300

    feat: add logError

--- logger.go
+++ logger.go
@@ -8,3 +8,7 @@ import (
 func logEvent(w io.Writer, e Event) {
 	fmt.Fprintln(w, formatEventLog(e))
 }
+
+func logError(w io.Writer, kind string, err error) {
+	fmt.Fprintln(w, "[ERROR]", kind, "error has occured but was ignored:", err)
+}
